4ea2a4a9a2e95e2bbcc4f9268d05be7169155116
Pol <paulcarva2@hotmail.com>
2024-04-30 16:55:48 +0200

    Commit 1 de prueba 2

---
+++ src/prueban2/PruebaN2.java
@@ -0,0 +1,14 @@
+
+package prueban2;
+
+public class PruebaN2 {
+
+    public static void main(String[] args) {
+        int a = 10;
+        
+        int b = 9;
+        
+        System.out.println("alo" + a);
+    }
+    
+}
